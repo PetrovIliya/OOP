a4a911ec26dbb98f296249a60b88c7351411c22b
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-02-23 01:19:32 +0300

    Залил replace

---
+++ lw1/replace/replace/replace.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string ReplaceString(const std::string& subject,
+	const std::string& searchString, const std::string& replacementString)
+{
+	size_t pos = 0;
+	std::string result;
+	while (pos < subject.length())
+	{
+		size_t foundPos = subject.find(searchString, pos);
+		result.append(subject, pos, foundPos - pos);
+		if (foundPos == std::string::npos)
+		{
+			break;
+		}
+		result.append(replacementString);
+		pos = foundPos + searchString.length();
+	}
+	return result;
+}
+
+void CopyFileWithReplace(std::istream& input, std::ostream& output,
+	const std::string& searchString, const std::string& replacementString)
+{
+	std::string line;
+
+	while (std::getline(input, line))
+	{
+		output << ReplaceString(line, searchString, replacementString) << "\n";
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		std::cout << "Invalid argument count\n"
+			<< "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>\n";
+		return 1;
+	}
+
+	std::ifstream inputFile;
+	inputFile.open(argv[1]);
+
+	std::ofstream outputFile;
+	outputFile.open(argv[2]);
+
+	std::string search = argv[3];
+	std::string replace = argv[4];
+
+	CopyFileWithReplace(inputFile, outputFile, search, replace);
+
+	if (!outputFile.flush())
+	{
+		std::cout << "Failed to save data on disk\n";
+		return 1;
+	}
+
+	return 0;
+}
