694adcfa8edec87b36f7ab9a73fdce1bf17169ff
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-02-26 01:30:47 +0300

    Реализовал первую версию конвертора

--- lw1/radix/radix.cpp
+++ lw1/radix/radix.cpp
@@ -4,6 +4,14 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
+#include <optional>
+
+struct ErrorCodes
+{
+    static const int WRONG_SYMBOLS_ERR = 1;
+    static const int NO_NUMBER_ERR = 2;
+    static const int CONVERT_ERROR = 3;
+};
 
 class CConvertor
 {
@@ -14,19 +22,45 @@ public:
         InitNumbers();
     }
 
-    std::string convert(int from, int to, std::string number)
+    std::string convert(int from, int to, std::string number, bool &wasError)
     {
+        toUpperCase(number);
         if (!IsValidNumber(number, from))
         {
-            std::cout << "Number contains invalid characters or out of radix range";
-            throw std::exception();
+            errorCode = ErrorCodes::WRONG_SYMBOLS_ERR;
+            wasError = true;
+            return "";
+        }
+        int decNumber = StringToInt(number, from, wasError);
+
+        if (wasError)
+        {
+            return "";
         }
 
-        return "a";
+        return IntToString(decNumber, to, wasError);
+    }
+
+    void PrintError()
+    {
+        std::cout << "test";
+        switch (errorCode)
+        {
+            case ErrorCodes::WRONG_SYMBOLS_ERR:
+                std::cout << "Number Contains wrong symbols or symbols out of radix range\n";
+                break;
+            case ErrorCodes::NO_NUMBER_ERR:
+                std::cout << "Emptmy number\n";
+                break;
+            case ErrorCodes::CONVERT_ERROR:
+                std::cout << "An error occurred while converting\n";
+                break;
+        }
     }
 
 private:
     std::vector<char> numbers;
+    int errorCode = 0;
 
     void InitNumbers()
     {
@@ -34,28 +68,85 @@ private:
         {
             numbers.push_back(i + '0');
         }
-        for (char ch = 'a'; ch <= 'z'; ch++)
+        for (char ch = 'A'; ch <= 'Z'; ch++)
         {
             numbers.push_back(ch);
         }
     }
 
+    void toUpperCase(std::string& string)
+    {
+        std::transform(string.begin(), string.end(), string.begin(), ::toupper);
+    }
+
     int StringToInt(const std::string& str, int radix, bool& wasError)
     {
-        if (radix <= 10)
+        int stringLength = str.size();
+        if (stringLength == 0)
+        {
+            wasError = true;
+            errorCode = ErrorCodes::NO_NUMBER_ERR;
+            return -1;
+        }
+        if (radix == 10)
         {
             return std::stoi(str);
         }
-        else
-        {
 
+        return ConvertToDec(str, stringLength, radix);
+    }
+
+    int ConvertToDec(std::string str, int stringLength, int radix)
+    {
+        std::vector <int> intNumbers = GetNumericList(str);
+        int basis = 0;
+        int result = 0;
+        for (int i = stringLength - 1; i >= 0; i--)
+        {
+            result += intNumbers[i] * pow(radix, basis);
+            if (basis == 0)
+            {
+                basis = 1;
+            }
+            else
+            {
+                basis <<= 1;
+            }
         }
-        return 1;
+
+        return result;
+    }
+
+    std::vector <int> GetNumericList(std::string str)
+    {
+        std::vector<char>::iterator it;
+        std::vector <int> intNumbers;
+        int position;
+        for (char ch : str)
+        {
+            it = std::find(numbers.begin(), numbers.end(), ch);
+            position = std::distance(numbers.begin(), it);
+            intNumbers.push_back(position);
+        }
+
+        return intNumbers;
     }
 
     std::string IntToString(int n, int radix, bool& wasError)
     {
-        return "a";
+        char str[30];
+        int errCode;
+
+        errCode = _itoa_s(n, str, radix);
+        
+        if (errCode != 0)
+        {
+            wasError = true;
+            errorCode = ErrorCodes::CONVERT_ERROR;
+            return "";
+        }
+
+        return str;
     }
 
     bool IsValidNumber(const std::string& str, int radix)
@@ -98,16 +189,16 @@ int main(int argc, char* argv[])
     args.numberSystemFrom = atoi(argv[1]);
     args.numberSystemTo = atoi(argv[2]);
     args.number = argv[3];
+    bool wasError = false;
     CConvertor convertor;
-
-    try
+    std::string result = convertor.convert(args.numberSystemFrom, args.numberSystemTo, args.number, wasError);
+    if (wasError)
     {
-        convertor.convert(args.numberSystemFrom, args.numberSystemTo, args.number);
-    }
-    catch (const std::exception&)
-    {
+        convertor.PrintError();
         return 1;
-    };
+    }
+
+    std::cout << result << std::endl;
 
     return 0;
 }
