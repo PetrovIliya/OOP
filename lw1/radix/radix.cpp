61f8880be44529e222d543541419176144b45664
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-03-15 15:57:12 +0300

    Рефакторинг radix

--- lw1/radix/radix.cpp
+++ lw1/radix/radix.cpp
@@ -17,13 +17,6 @@ struct Args
     std::string number;
 };
 
-void CheckNumberForEmptiness(std::string number)
-{
-    if (number.size() == 0)
-    {
-        throw std::exception("Number is empty\n");
-    }
-}
 
 std::string ToUpperCase(std::string string)
 {
@@ -31,7 +24,30 @@ std::string ToUpperCase(std::string string)
     return string;
 }
 
-bool IsSummOverflowed(int arg1, int arg2)
+void CheckNumberForCorrectRadixRange(std::string number, int radix)
+{
+    if (number.empty())
+    {
+        throw std::invalid_argument("Number is empty");
+    }
+    char firstSymbol = number[0];
+    if (firstSymbol == '-')
+    {
+        number.erase(0, 1);
+    }
+    for (char symbol : number)
+    {
+        std::vector<char>::const_iterator it;
+        it = std::find(possibleNumbersOfNumberSystem.begin(), possibleNumbersOfNumberSystem.end(), symbol);
+        int position = (int)std::distance(possibleNumbersOfNumberSystem.begin(), it);
+        if (it == possibleNumbersOfNumberSystem.end() || position >= radix)
+        {
+            throw std::invalid_argument("Number contains out of radix range symbols\n");
+        }
+    }
+}
+
+bool IsPositiveSummOverflowed(int arg1, int arg2)
 {
     if (INT_MAX - arg1 - arg2 < 0)
     {
@@ -62,46 +78,41 @@ bool IsMultOverflowed(int arg1, int arg2)
     return false;
 }
 
-void OverflowControl(int radix, unsigned int oldResult, int iterableNumber, bool isConversionNumberNegative)
-{
-    unsigned int tmpResult = oldResult;
-    if (isConversionNumberNegative && tmpResult != 0)
-    {
-        tmpResult--;
-    }
-    if (IsMultOverflowed(radix, tmpResult))
-    {
-        throw std::exception("Overflow detected");
-    }
-    int multResult = radix * oldResult;
-    if (isConversionNumberNegative && multResult != 0)
-    {
-        multResult--;
-    }
-    if (IsSummOverflowed(multResult, iterableNumber))
-    {
-        throw std::exception("Overflow detected");
-    }
-}
-
 unsigned int StringToInt(const std::string& str, int radixFrom, bool isConversionNumberNegative)
 {
-    if (str.size() == 0)
+    if (str.empty())
     {
-        throw std::exception("An error occurred during program execution\n");
+        throw std::invalid_argument("An error occurred during program execution\n");
     }
-    if (radixFrom == 10)
-    {
-        return std::stoi(str);
-    }
+    CheckNumberForCorrectRadixRange(str, radixFrom);
     std::vector<char>::const_iterator it;
     unsigned int result = 0;
+    unsigned int tmpResult;
     int intNumber;
     for (char ch : str)
     {
         it = std::find(possibleNumbersOfNumberSystem.begin(), possibleNumbersOfNumberSystem.end(), ch);
         intNumber = (int)std::distance(possibleNumbersOfNumberSystem.begin(), it);
-        OverflowControl(radixFrom, result, intNumber, isConversionNumberNegative);
+
+        tmpResult = result;
+        if (isConversionNumberNegative && tmpResult != 0)
+        {
+            tmpResult--;
+        }
+        if (IsMultOverflowed(radixFrom, tmpResult))
+        {
+            throw std::overflow_error("Overflow detected");
+        }
+        int multResult = radixFrom * result;
+        if (isConversionNumberNegative && multResult != 0)
+        {
+            multResult--;
+        }
+        if (IsPositiveSummOverflowed(multResult, intNumber))
+        {
+            throw std::overflow_error("Overflow detected");
+        }
+
         result = (result * radixFrom) + intNumber;
     }
     return result;
@@ -112,21 +123,20 @@ std::string IntToString(unsigned int number, int radixTo)
 
     if (radixTo == 0)
     {
-        throw std::exception("Radix can't be zero\n");
+        throw std::invalid_argument("Radix can't be zero\n");
     }
-    if (radixTo == 10)
-    {
-        return std::to_string(number) ;
-    }
+
     std::vector<unsigned int> remindersOfDivision;
     std::string result;
-    while ((int)number >= radixTo)
+
+    while (number >= (unsigned)radixTo)
     {
         remindersOfDivision.push_back(possibleNumbersOfNumberSystem[number % radixTo]);
         number /= radixTo;
+
     }
     remindersOfDivision.push_back(possibleNumbersOfNumberSystem[number]);
-    for (int i = remindersOfDivision.size() - 1; i >= 0; i--)
+    for (int i = (int)remindersOfDivision.size() - 1; i >= 0; i--)
     {
         result += remindersOfDivision[i];
     }
@@ -134,29 +144,13 @@ std::string IntToString(unsigned int number, int radixTo)
     return result;
 }
 
-void CheckNumberForCorrectRadixRange(std::string number, int radix)
-{
-    CheckNumberForEmptiness(number);
-    char firstSymbol = number[0];
-    if (firstSymbol == '-')
-    {
-        number.erase(0, 1);
-    }
-    for (char symbol : number)
-    {
-        std::vector<char>::const_iterator it;
-        it = std::find(possibleNumbersOfNumberSystem.begin(), possibleNumbersOfNumberSystem.end(), symbol);
-        int position = (int)std::distance(possibleNumbersOfNumberSystem.begin(), it);
-        if (it == possibleNumbersOfNumberSystem.end() || position >= radix)
-        {
-            throw std::exception("Number contains out of radix range symbols\n");
-        }
-    }
-}
 
 std::string Convert(int from, int to, std::string number)
 {
-    CheckNumberForEmptiness(number);
+    if (number.empty())
+    {
+        throw std::invalid_argument("Number is empty");
+    }
     bool isConversionNumberNegative = false; 
     if (number[0] == '-')
     {
@@ -182,21 +176,38 @@ bool IsNumeric(const std::string& string)
     return string.find_first_not_of(templateOfNumbers, offset) == std::string::npos;
 }
 
+bool IsNumberBetween(int number, int from, int to)
+{
+    if (number < from || number > to)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 Args ParseArgs(int argc, char* argv[])
 {
     if (argc != 4)
     {
-       throw  std::exception("Wrong quantity of parameters.\nUsage: radix.exe <numeral system from> <number system to> <number>\n");
+        throw  std::runtime_error("Wrong quantity of parameters.\nUsage: radix.exe <numeral system from> <number system to> <number>\n");
     }
-    if (!IsNumeric(argv[1]) || !IsNumeric(argv[2]) || atoi(argv[1]) < 2 || atoi(argv[2]) < 2 || atoi(argv[1]) > 36 || atoi(argv[2]) > 36)
+
+    if (!IsNumeric(argv[1]) || !IsNumeric(argv[2]))
     {
-        throw  std::exception("Wrong numeral system. Numeral system should be between 2 and 36\n");
+        throw  std::invalid_argument("Wrong numeral system. Numeral system should be a number\n");
     }
+
     Args args;
     args.numeralSystemFrom = atoi(argv[1]);
     args.numeralSystemTo = atoi(argv[2]);
     args.number = ToUpperCase(argv[3]);
-    CheckNumberForCorrectRadixRange(args.number, args.numeralSystemFrom);
+
+    if (!IsNumberBetween(args.numeralSystemFrom, 2, 36) || !IsNumberBetween(args.numeralSystemTo, 2, 36))
+    {
+        throw  std::invalid_argument("Wrong numeral system. Numeral system should be between 2 and 36\n");
+    }
+   
     return args;
 }
 
@@ -209,10 +220,15 @@ int main(int argc, char* argv[])
         std::string result = Convert(args.numeralSystemFrom, args.numeralSystemTo, args.number);
         std::cout << result << std::endl;
     }
-    catch(const std::exception & e)
+    catch (std::invalid_argument & e)
     {
         std::cout << e.what() << '\n';
         return 1;
-    };
+    }
+    catch (std::runtime_error & e)
+    {
+        std::cout << e.what() << '\n';
+        return 1;
+    }
     return 0;
 }
--- lw1/radix/test.bat
+++ lw1/radix/test.bat
@@ -9,36 +9,42 @@ echo test 1 passed
 
 rem При переводе из 16ой сс в 10ю числа F ожидается результат - 15
 %PROGRAM% 16 10 F > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_16_to_10_f.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 2 passed
 
 rem При переводе из 10ой сс в 16ю числа 15 ожидается результат - f
 %PROGRAM% 10 16 15 > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_10_to_16_15.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 3 passed
 
 rem При переводе из 16ой сс в 10ю числа 0 ожидается результат - 0
 %PROGRAM% 16 10 0 > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_16_to_10_0.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 4 passed
 
 rem При переводе из 16ой сс в 10ю числа -f ожидается результат - -15
 %PROGRAM% 16 10 -f > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_16_to_10_-f.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 5 passed
 
 rem При переводе из 16ой сс в 10ю числа 7FFFFFFF ожидается результат - 2147483647 (INT_MAX)
 %PROGRAM% 16 10 7FFFFFFF > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_16_to_10_7FFFFFFF.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 6 passed
 
 rem При переводе из 16ой сс в 10ю числа -80000000 ожидается результат - -2147483648 (INT_MIN)
 %PROGRAM% 16 10 -80000000 > "%TEMP%\output.txt"
+if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" "%TESTDIR%\from_16_to_10_-80000000.txt" > nul
 if ERRORLEVEL 1 goto err
 echo test 7 passed
