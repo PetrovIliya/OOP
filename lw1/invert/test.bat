9a97968bd493481d465b45ec0fcf19c41175b66c
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-03-18 00:45:12 +0300

    Доработал тесты

--- lw1/invert/test.bat
+++ lw1/invert/test.bat
@@ -1,10 +1,30 @@
 SET PROGRAM="%~1"
 set TESTDIR="%~2"
 
+REM no parameters
+%PROGRAM% && goto err
+echo test 1 passed
+
+REM if output not exist, expeting errorlevel != 
+%PROGRAM% MissingFile.txt && goto err
+echo Test 2 passed
+
 REM det != 0
 %PROGRAM% %TESTDIR%\test1.txt > %TEMP%\output.txt || goto err
 fc.exe %TEMP%\output.txt %TESTDIR%\test1_right_res.txt > nul || goto err
-echo test 1 passed
+echo test 3 passed
+
+REM det = 0
+%PROGRAM% %TESTDIR%\test2.txt && goto err
+echo Test 4 passed
+
+REM empty file
+ %PROGRAM% %TESTDIR%\input_empty_file.txt && goto err
+ echo test 5 passed
+
+ REM matrix with letters
+%PROGRAM% %TESTDIR%\input_matrix_with_letters.txt && goto err
+echo test 6 passed
 
 REM I successfully tested the tests
 echo All tests passed successfuly
