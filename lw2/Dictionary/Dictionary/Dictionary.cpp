5a0197ef10b79c73799f1c8b4ebfc0f9dafa5b44
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-05-10 18:36:59 +0300

    Начало разработки Dictionary

--- lw2/Dictionary/Dictionary/Dictionary.cpp
+++ lw2/Dictionary/Dictionary/Dictionary.cpp
@@ -2,29 +2,88 @@
 #include <string>
 #include <exception>
 #include <fstream>
+#include <map>
 
 using namespace std;
+using Dictionary = map<string, string>;
 
-string GetFileName(int argc, char* argv[])
+const string TMP_FILE_NAME = "tmp.txt";
+const string TRANSLATE_DELIMITER = " - ";
+const string EXIT_STRING = "...";
+
+struct WordTranslate
 {
-	if (argc < 2)
+	string word;
+	string translate;
+};
+
+string GetUserOrTmpFileName(int argc, char* argv[])
+{
+	if (argc < 2 || argv[1] == "")
 	{
-		throw new invalid_argument("No file name");
+		return TMP_FILE_NAME;
 	}
 
 	return argv[1];
 }
 
-fstream GetExistingOrCreateNewFile()
+WordTranslate CreateWordTranslateFromLine(const string& line)
 {
+	WordTranslate wordTranslate;
+	auto wordEnd = line.find(TRANSLATE_DELIMITER);
+	wordTranslate.word = line.substr(0, wordEnd);
 
+	auto translateStart = wordEnd + TRANSLATE_DELIMITER.length();
+	wordTranslate.translate = line.substr(translateStart);
+	
+	return wordTranslate;
+}
+
+Dictionary CreateDictionaryFromFile(const string& fileName)
+{
+	ifstream file(fileName);
+	Dictionary dictionary;
+	if (!file.is_open() || file.eof())
+	{
+		return dictionary;
+	}
+
+	string line;
+	while (getline(file, line))
+	{
+		WordTranslate wordTranslate = CreateWordTranslateFromLine(line);
+		dictionary[wordTranslate.word] = wordTranslate.translate;
+	}
+
+	return dictionary;
+}
+
+void StartDialog(Dictionary dictionary)
+{
+	Dictionary newWords;
+	string wordForTranslate;
+
+	while (cin >> wordForTranslate)
+	{
+		cout << wordForTranslate << endl;
+
+		if (wordForTranslate == EXIT_STRING)
+		{
+			break;
+		}
+
+
+	}
 }
 
 int main(int argc, char* argv[])
 {
+	setlocale(LC_ALL, "rus");
 	try
 	{
-		string fileName = GetFileName(argc, argv);
+		string fileName = GetUserOrTmpFileName(argc, argv);
+		Dictionary dictionary = CreateDictionaryFromFile(fileName);
+		StartDialog(dictionary);
 	}
 	catch (const exception& e)
 	{
@@ -33,5 +92,4 @@ int main(int argc, char* argv[])
 	}
 
 	return 0;
-
 }
