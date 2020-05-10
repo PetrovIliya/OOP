7c955b5f50193adb9e177be8d953096b2b0ba4a4
iliya-petrov <52219078+PetrovIliya@users.noreply.github.com>
2020-05-10 15:55:27 +0300

    test
    test

---
+++ lw2/Dictionary/Dictionary/Dictionary.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <exception>
+#include <fstream>
+
+using namespace std;
+
+string GetFileName(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		throw new invalid_argument("No file name");
+	}
+
+	return argv[1];
+}
+
+fstream GetExistingOrCreateNewFile()
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		string fileName = GetFileName(argc, argv);
+	}
+	catch (const exception& e)
+	{
+		cout << e.what() << '\n';
+		return 1;
+	}
+
+	return 0;
+
+}
